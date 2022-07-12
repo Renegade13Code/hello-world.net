ab7d800bfc3f8c2e7bfd18db3458543e61ff5032
Brandon <brandon16scott@gmail.com>
2022-07-12 15:57:39 +0200

    changed cert

---
+++ HelloWorld/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld
+{
+    [Route(".well-known/pki-validation/")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "B1E3BC186832A4EE80457EBF4BADD3B93D8574A3ABE59C898C83B0E8D690F65E\nssl.com\n20220711";
+        }
+    }
+}
